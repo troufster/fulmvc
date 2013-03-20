ec03723562c7656d9920e159c2ba1b0f50704ef4
majorbyte <majorbyte@gmail.com>
2013-03-20 20:54:28 +0100

    Working on MVC combined with HTML/Javascript/CSS3
    Added Areas to the project.... see point 4 on the index page for a link
    to read more about Areas.
    But I figured it would be easy to for everyone who'd use this solution
    to have his own area....

---
+++ fulmvc/Areas/Majorbyte/Controllers/MajorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fulmvc.Areas.Majorbyte.Controllers
+{
+    public class MajorController : Controller
+    {
+        //
+        // GET: /Majorbyte/Major/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ fulmvc/Areas/Majorbyte/MajorbyteAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace fulmvc.Areas.Majorbyte
+{
+    public class MajorbyteAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Majorbyte";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Majorbyte_default",
+                "Majorbyte/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
